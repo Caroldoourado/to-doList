4a5bac61071c5899d81f677efaff0440027c0d4c
Caroldoourado29 <85802214+Caroldoourado@users.noreply.github.com>
2025-02-19 18:45:53 -0300

    Adicionei o localStorage para persistir a lista

--- script.js
+++ script.js
@@ -2,21 +2,38 @@ const taskInput = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTaskButton");
 const taskList = document.getElementById("taskList");
 
+let savedItems = JSON.parse(localStorage.getItem("items")) || [];
+
 function addTask(){
     const taskText = taskInput.value.trim();
 
     if(taskText !== ""){
+        savedItems.push(taskText);
+        localStorage.setItem("items", JSON.stringify(savedItems));
+        taskInput.value = "";    
         
+        loadItems();
+    }
+}
+
+function loadItems(){
+
+    taskList.innerHTML = "";
+
+    savedItems.forEach(itemText => {
         const taskItem = document.createElement("li");
 
         const taskTextSpan = document.createElement("span");
-        taskTextSpan.textContent = taskText;
+        taskTextSpan.textContent = itemText;
         
         const removeButton = document.createElement("button");
         removeButton.textContent = "Remover";
 
         removeButton.addEventListener("click", function(){
-            taskItem.remove();
+            const texto = taskTextSpan.textContent.trim();
+            savedItems = savedItems.filter(item => item !== texto);
+            localStorage.setItem("items", JSON.stringify(savedItems));
+            taskItem.remove(); 
         })
 
         taskTextSpan.addEventListener("click", function () {
@@ -26,11 +43,13 @@ function addTask(){
 
         taskItem.appendChild(taskTextSpan);
         taskItem.appendChild(removeButton);
-        taskList.appendChild(taskItem);
-        taskInput.value = "";
-    }
+        taskList.appendChild(taskItem);            
+        });
+
+    
 }
 
+loadItems();
 addTaskButton.addEventListener("click", function(){
     addTaskButton.style.backgroundColor = "rgb(244, 227, 75)";
 
